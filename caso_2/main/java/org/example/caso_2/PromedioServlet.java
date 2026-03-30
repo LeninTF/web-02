6081eb459c22f7ef06bc0f99e4a4a7fec8c53f2a
David Eduardo Peña Tinco <u23227035@utp.edu.pe>
2026-03-30 12:43:54 -0500

    Caso_2

---
+++ caso_2/main/java/org/example/caso_2/PromedioServlet.java
@@ -0,0 +1,36 @@
+package org.example.caso_2;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+@WebServlet("/PromedioServlet")
+public class PromedioServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        double c1 = Double.parseDouble(request.getParameter("c1"));
+        double c2 = Double.parseDouble(request.getParameter("c2"));
+        double c3 = Double.parseDouble(request.getParameter("c3"));
+
+        double promedio = (c1 + c2 + c3) / 3;
+
+        String estado;
+        if (promedio >= 11) {
+            estado = "Aprobado";
+        } else {
+            estado = "Desaprobado";
+        }
+
+        request.setAttribute("promedio", promedio);
+        request.setAttribute("estado", estado);
+
+        request.getRequestDispatcher("resultado.jsp").forward(request, response);
+    }
+}
---
+++ caso_2/main/webapp/resultado.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+  <title>Resultado</title>
+</head>
+<body>
+<h2>Resultado</h2>
+
+Promedio: ${promedio} <br>
+Estado: ${estado}
+
+<br><br>
+<a href="index.jsp">Volver</a>
+</body>
+</html>
